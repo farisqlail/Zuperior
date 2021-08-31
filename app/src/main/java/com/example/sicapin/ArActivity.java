f29c20342f3e63ad73fa9781a0ba14424d6a1de5
farisqlail <faris.riskilail@gmail.com>
2021-08-31 14:43:15 +0700

    ganti logo coba implement ar

---
+++ app/src/main/java/com/example/sicapin/ArActivity.java
@@ -0,0 +1,29 @@
+package com.example.sicapin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class ArActivity extends AppCompatActivity {
+
+    WebView webView;
+    WebSettings webSettings;
+    String MODEL_URL="https://novelfaris.000webhostapp.com/story/arView.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_ar);
+
+        WebView myWebView = (WebView) findViewById(R.id.arView);
+        webSettings = myWebView.getSettings();
+
+        myWebView.loadUrl(MODEL_URL);
+        webSettings.setJavaScriptEnabled(true);
+    }
+}
