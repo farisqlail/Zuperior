83b35559435d84139f0a43114abdde28157df434
Fadil <fadilfdl2710@gmail.com>
2021-09-09 17:35:14 +0700

    push branch Faris

--- app/src/main/java/com/example/sicapin/Fragmen/Home.java
+++ app/src/main/java/com/example/sicapin/Fragmen/Home.java
@@ -1,67 +1,87 @@
 package com.example.sicapin.Fragmen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sicapin.Activity.ArActivity;
+import com.example.sicapin.Activity.StartQuiz;
 import com.example.sicapin.R;
+import com.example.sicapin.tes;
+import com.example.sicapin.onBoard;
+import com.example.sicapin.tes;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Home#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Home extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    protected CardView cvArtikels, cvCameras, cvQuizs, cvProfiles;
 
     public Home() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HOme.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
-        Home fragment = new Home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        // EndInflate
+
+        //FindViews
+        cvArtikels = (CardView) view.findViewById(R.id.cvArtikel);
+        cvCameras  = (CardView) view.findViewById(R.id.cvCamera);
+        cvProfiles = (CardView) view.findViewById(R.id.cvProfile);
+        cvQuizs    = (CardView) view.findViewById(R.id.cvQuiz);
+        //EndFindViews
+
+
+        //Listener
+
+        cvArtikels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), tes.class);
+                startActivity(intent);
+            }
+        });
+
+        cvProfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity().getApplicationContext(), tes.class);
+                startActivity(intent);
+
+            }
+        });
+
+        cvCameras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), ArActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cvQuizs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity().getApplicationContext(), tes.class);
+                startActivity(intent);
+
+            }
+        });
+        //EndListener
+
         return view;
     }
 }
