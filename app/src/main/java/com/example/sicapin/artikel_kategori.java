878e3bbb53f5ba12dc6fc15513074350ab211a0c
Fadil <fadilfdl2710@gmail.com>
2021-09-19 10:40:55 +0700

    ndelok anime sek

---
+++ app/src/main/java/com/example/sicapin/artikel_kategori.java
@@ -0,0 +1,14 @@
+package com.example.sicapin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class artikel_kategori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artikel_kategori);
+    }
+}
