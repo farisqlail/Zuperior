fd65c40261776cb057a0b72cec0aa38cc52018b9
Fadil <fadilfdl2710@gmail.com>
2021-09-23 22:59:46 +0700

    Break make kategori matematika

---
+++ app/src/main/java/com/example/sicapin/Activity/kat_artikel_mtk.java
@@ -0,0 +1,16 @@
+package com.example.sicapin.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sicapin.R;
+
+public class kat_artikel_mtk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kat_artikel_mtk);
+    }
+}
