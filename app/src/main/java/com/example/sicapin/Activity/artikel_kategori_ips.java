c7f33dd8aa247451eb4b3ee527977b6584f149f0
Fadil <fadilfdl2710@gmail.com>
2021-09-28 19:06:26 +0700

    break makan

---
+++ app/src/main/java/com/example/sicapin/Activity/artikel_kategori_ips.java
@@ -0,0 +1,16 @@
+package com.example.sicapin.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sicapin.R;
+
+public class artikel_kategori_ips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artikel_kategori_ips);
+    }
+}
