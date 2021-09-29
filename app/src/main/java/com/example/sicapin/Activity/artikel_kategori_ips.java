93434ce2eaaeacdb9c018d4a39fac5f4b24b95a3
Fadil <fadilfdl2710@gmail.com>
2021-09-29 11:12:29 +0700

    artikel kategori ips (done)

--- app/src/main/java/com/example/sicapin/Activity/artikel_kategori_ips.java
+++ app/src/main/java/com/example/sicapin/Activity/artikel_kategori_ips.java
@@ -3,14 +3,24 @@ package com.example.sicapin.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.sicapin.R;
+import com.example.sicapin.itemObjectClass.item_artikel_kat;
 
 public class artikel_kategori_ips extends AppCompatActivity {
+
+    ListView lv_kat_artikel;
+    String[]  tv_lv     = {"Tanpa Judul", "Tanpa Judul", "Tanpa Judul", "Tanpa Judul", "Tanpa Judul", "Tanpa Judul", "Tanpa Judul", "Tanpa Judul"};
+    Integer[] ic_lv     = {R.drawable.kataroff, R.drawable.kataroff, R.drawable.kataroff, R.drawable.kataroff, R.drawable.kataroff, R.drawable.kataroff, R.drawable.kataroff, R.drawable.kataroff};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artikel_kategori_ips);
+        item_artikel_kat adapter = new item_artikel_kat(artikel_kategori_ips.this, tv_lv, ic_lv);
+        lv_kat_artikel = findViewById(R.id.lv_artikel);
+
+        lv_kat_artikel.setAdapter(adapter);
     }
 }
